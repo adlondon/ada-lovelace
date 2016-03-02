385b1f680a61c1a68d532afd72c81c26145ab4ca
Patrick Coleman <colemanrpatrick@gmail.com>
2016-03-02 15:20:49 -0500

    styles bitch

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 var weatherTemplate ={
   weather: ['<div class="weatherInfo">',
       '<p class="cityName"><%= name %></p>',
-      '<p class="temp"><%= temp %></p>',
+      '<p class="temp"><%= temp %>°F</p>',
       '<p class="description"><%= description %></p>',
     '</div>'
   ].join('')
@@ -15,6 +15,7 @@ var weatherTemplate ={
 var pages = {
   init: function () {
     pages.getLocation();
+    pages.timeToDom();
   },
 
 
@@ -29,11 +30,10 @@ var pages = {
       url: pages.buildForecastUrl(ourCoords.coords),
       dataType: "jsonp",
       success: function (data) {
-
         console.log("SUCCESS", data);
-        pages.addWeatherToDom(pages.buildWeatherData(data))
+        pages.addWeatherToDom(pages.buildWeatherData(data));
       }
-    })
+    });
   },
 
   addWeatherToDom: function (data) {
@@ -42,26 +42,32 @@ var pages = {
     $('.weatherContainer').html(weatherTmpl(data));
   },
 
+  timeToDom: function(){
+    pages.makeTime($('.timeContainer'));
+  },
+  makeTime: function($target) {
+      $target.html('');
+      setInterval(function() {
+        var timeput= moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
+        var htmlstr = "<span class=time>" + timeput + "</span>";
+        $target.html(htmlstr); }, 1000);
+    },
 
 
-// addToDom: function($target) {
-//     $target.html('');
-//     setInterval(function() {
-//       var timeput= moment(Date.now()).format('MMMM Do YYYY, h:mm:ss a');
-//       var htmlstr = "<span class=time>"+ timeput + "</span>";
-//       $target.html(htmlstr); }, 1000);
-
-  buildWeatherData: function (el) {
-    console.log("ARRRRRR", el)
+  buildWeatherData: function (el){
+    console.log("ARRRRRR", el);
       return {
         name: el.name,
-        temp: el.main.temp,
-        description: el.weather[0].description
+        temp: Math.round(el.main.temp),
+        description: el.weather[0].description,
       };
   },
 
 
-buildForecastUrl: function (coords) {
-return "http://api.openweathermap.org/data/2.5/weather?lat=" + coords.latitude + "&lon=" + coords.longitude + "&APPID=d4bb61d47f0ff2fa54c2fc465d28a37a";
+buildForecastUrl: function (coords){
+return "http://api.openweathermap.org/data/2.5/weather?lat=" + coords.latitude + "&lon=" + coords.longitude + "&APPID=d4bb61d47f0ff2fa54c2fc465d28a37a"+
+"&units=imperial";
 }
-}
+};
+
+///////////
